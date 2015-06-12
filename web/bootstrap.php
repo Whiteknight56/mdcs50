840e3b61f07ffdf591eebb6239a9bc3d556288a5
Whiteknight56 <damusdon@gmail.com>
2015-06-11 20:39:32 -0700

    renamed files to php

---
+++ web/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+$website_settings  = [
+    'title' => 'Game of life',
+    'page_title' => 'Game',
+    'home' => 'Never',
+    'page_description'=>'Game',
+    'copyright' => 'Copyright &copy; Your Website %s',
+];
--- web/portfolio-4-col.php
+++ web/portfolio-4-col.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+require_once 'bootstrap.php';
+?>
 
 <head>
 
@@ -9,7 +12,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Todays Business - Start Bootstrap Template</title>
+    <title><?= $website_settings['title'] ?></title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -42,37 +45,37 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+                <a class="navbar-brand" href="index.php"><?= $website_settings['home'] ?></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a href="about.html">About</a>
+                        <a href="about.php">About</a>
                     </li>
                     <li>
-                        <a href="services.html">Services</a>
+                        <a href="services.php">Services</a>
                     </li>
                     <li>
-                        <a href="contact.html">Contact</a>
+                        <a href="contact.php">Contact</a>
                     </li>
                     <li class="dropdown active">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
-                                <a href="portfolio-1-col.html">1 Column Portfolio</a>
+                                <a href="portfolio-1-col.php">1 Column Portfolio</a>
                             </li>
                             <li>
-                                <a href="portfolio-2-col.html">2 Column Portfolio</a>
+                                <a href="portfolio-2-col.php">2 Column Portfolio</a>
                             </li>
                             <li>
-                                <a href="portfolio-3-col.html">3 Column Portfolio</a>
+                                <a href="portfolio-3-col.php">3 Column Portfolio</a>
                             </li>
                             <li class="active">
                                 <a href="portfolio-4-col.php">4 Column Portfolio</a>
                             </li>
                             <li>
-                                <a href="portfolio-item.html">Single Portfolio Item</a>
+                                <a href="portfolio-item.php">Single Portfolio Item</a>
                             </li>
                         </ul>
                     </li>
@@ -80,13 +83,13 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
-                                <a href="blog-home-1.html">Blog Home 1</a>
+                                <a href="blog-home-1.php">Blog Home 1</a>
                             </li>
                             <li>
-                                <a href="blog-home-2.html">Blog Home 2</a>
+                                <a href="blog-home-2.php">Blog Home 2</a>
                             </li>
                             <li>
-                                <a href="blog-post.html">Blog Post</a>
+                                <a href="blog-post.php">Blog Post</a>
                             </li>
                         </ul>
                     </li>
@@ -94,19 +97,19 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
-                                <a href="full-width.html">Full Width Page</a>
+                                <a href="full-width.php">Full Width Page</a>
                             </li>
                             <li>
-                                <a href="sidebar.html">Sidebar Page</a>
+                                <a href="sidebar.php">Sidebar Page</a>
                             </li>
                             <li>
-                                <a href="faq.html">FAQ</a>
+                                <a href="faq.php">FAQ</a>
                             </li>
                             <li>
-                                <a href="404.html">404</a>
+                                <a href="404.php">404</a>
                             </li>
                             <li>
-                                <a href="pricing.html">Pricing Table</a>
+                                <a href="pricing.php">Pricing Table</a>
                             </li>
                         </ul>
                     </li>
@@ -123,13 +126,13 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Four Column Portfolio
-                    <small>Subheading</small>
+                <h1 class="page-header"><?= $website_settings['title'];?>
+                    <small><?= $website_settings['page_title'] ?></small>
                 </h1>
                 <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
+                    <li><a href="index.php">Home</a>
                     </li>
-                    <li class="active">Four Column Portfolio</li>
+                    <li class="active"><?= $website_settings['Page_description'] ?></li>
                 </ol>
             </div>
         </div>
@@ -142,28 +145,29 @@
 		        [
 			        'container_class' => 'col-md-3 img-portfolio',
 			        'img_class' => 'img-responsive img-hover',
-			        'portfolio_link' => 'portfolio-item.html',
+			        'portfolio_link' => 'portfolio-item.php',
 			        'img_link' => 'http://lorempixel.com/g/750/450',
 		        ],
 		        [
 			        'container_class' => 'col-md-3 img-portfolio',
 			        'img_class' => 'img-responsive img-hover',
-			        'portfolio_link' => 'portfolio-item.html',
+			        'portfolio_link' => 'portfolio-item.php',
 			        'img_link' => 'http://lorempixel.com/g/750/450',
 		        ],
 		        [
 			        'container_class' => 'col-md-3 img-portfolio',
 			        'img_class' => 'img-responsive img-hover',
-			        'portfolio_link' => 'portfolio-item.html',
+			        'portfolio_link' => 'portfolio-item.php',
 			        'img_link' => 'http://lorempixel.com/g/750/450',
 		        ],
 		        [
 			        'container_class' => 'col-md-3 img-portfolio',
 			        'img_class' => 'img-responsive img-hover',
-			        'portfolio_link' => 'portfolio-item.html',
+			        'portfolio_link' => 'portfolio-item.php',
 			        'img_link' => 'http://lorempixel.com/g/750/450',
 		        ],
 	        ];
+
 
 	        foreach ( $portfolio as $portfolio_item ) {
 		        ?>
@@ -172,6 +176,7 @@
 				        <img class="<?= $portfolio_item['img_class']; ?>" src="<?= $portfolio_item['img_link']; ?>" alt="">
 			        </a>
 		        </div>
+
 	            <?php
 	        }
 
@@ -182,22 +187,22 @@
         <!-- Projects Row -->
         <div class="row">
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
@@ -207,22 +212,22 @@
         <!-- Projects Row -->
         <div class="row">
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
             <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
+                <a href="portfolio-item.php">
                     <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
                 </a>
             </div>
@@ -267,7 +272,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2015</p>
+                    <p><?= $website_settings['copyright'] ?></p>
                 </div>
             </div>
         </footer>
